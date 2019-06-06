c7396a98ab2939a08181fe77d268a5e15804e50f
Andrew Bui <andrewbui130@hotmail.com>
2019-06-06 14:56:17 -0700

    Use express json

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "/public/css"));
 app.use("/js", express.static(__dirname + "/public/js"));
 app.use("/images", express.static(__dirname + "/public/images"));
+app.use(express.json());
 
 /**
- * Set up server
+ * Run server
  */
 const server = app.listen(2019, function () {
   const port = server.address().port;
