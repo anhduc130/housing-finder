69780ccfb631d2e985f6daca057cb0b9b10ec69a
Andrew Bui <andrewbui130@hotmail.com>
2019-06-06 13:27:58 -0700

    Fix credentials

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express"),
-const connection = mysql.createConnection({host: "localhost", user: "dbuser", password: "s3kreee7", database: "housing_db"});
+const connection = mysql.createConnection({host: "localhost", user: "root", password: "root", database: "housing_finder"});
 const app = express();
 const mysql = require("mysql");
 
