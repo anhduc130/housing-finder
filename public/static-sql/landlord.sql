b48fc72e7c3a63ddf389bad5c90ed3d38503ef72
Andrew Bui <andrewbui130@hotmail.com>
2019-06-07 18:55:48 -0700

    Update landlord.sql

--- public/static-sql/landlord.sql
+++ public/static-sql/landlord.sql
@@ -2,6 +2,7 @@ CREATE TABLE landlord
 (
     landlord_id varchar(128) PRIMARY KEY,
     landlord_name varchar(20) NOT NULL,
-    email varchar(20) NOT NULL UNIQUE,
-    phone_number varchar(12)
+    landlord_email varchar(20) NOT NULL UNIQUE,
+    landlord_password varchar(128) NOT NULL,
+    landlord_phone_number varchar(12)
 );
