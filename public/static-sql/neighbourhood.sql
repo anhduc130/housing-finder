3abf03c3f417f043cbb4592850ae5c6e6ec66a5d
akwan224 <alvinkwan24@gmail.com>
2019-06-07 18:46:37 -0700

    tested neighborhood

--- public/static-sql/neighbourhood.sql
+++ public/static-sql/neighbourhood.sql
@@ -1,7 +1,7 @@
 CREATE TABLE neighbourhood
 (
     postal_code varChar (20) NOT NULL PRIMARY KEY,
-    neighbourhood_name varChar(20) NOT NULL,
+    neighbourhood_name varChar(40) NOT NULL,
     city_id int NOT NULL,
     FOREIGN KEY (city_id) references city(city_id)
 );
@@ -14,13 +14,11 @@ INSERT INTO neighbourhood VALUES ("V5P", "Victoria-Fraserview", "0001");
 INSERT INTO neighbourhood VALUES ("V5S", "Renfrew-Killarney", "0001");
 
 -- East Vancouver: 
-
 INSERT INTO neighbourhood VALUES ("V5L", "Grandview-Woodland", "0001");
 INSERT INTO neighbourhood VALUES ("V5K", "Hastings-Sunrise", "0001");
 INSERT INTO neighbourhood VALUES ("V5N", "Kensington-Cedar Cottage", "0001");
 INSERT INTO neighbourhood VALUES ("V5R", "Renfrew-Collingwood", "0001");
 INSERT INTO neighbourhood VALUES ("V5Y", "MountPleasant-Hillcrest-Oakridge", "0001");
-INSERT INTO neighbourhood VALUES ("V6A", "Strathcona", "0001");
 
 -- West Vancouver
 INSERT INTO neighbourhood VALUES ("V6T", "University Endowment Lands", "0001");
@@ -33,7 +31,7 @@ INSERT INTO neighbourhood VALUES ("V6J", "Shaughnessy-KitsPoint", "0001");
 INSERT INTO neighbourhood VALUES ("V6L", "Arbutus Ridge", "0001");
 
 -- Central Vancouver: 
-INSERT INTO neighbourhood VALUES ("V6N", "West End", "0001");
-INSERT INTO neighbourhood VALUES ("V6A", "Chinatown-DowntownEastside", "0001");
+INSERT INTO neighbourhood VALUES ("V6G", "West End", "0001");
+INSERT INTO neighbourhood VALUES ("V6A", "Chinatown-DowntownEastside-Strathcona", "0001");
 INSERT INTO neighbourhood VALUES ("V6B", "Downtown-Gastown-Coalharbour" , "0001");
 INSERT INTO neighbourhood VALUES ("V6Z", "Yaletown" , "0001");
