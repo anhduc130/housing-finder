adc3703d3494349c0821f5beb05076010e0ec5e7
Andrew Bui <andrewbui130@hotmail.com>
2019-06-07 18:40:10 -0700

    Change landlord_id to varchar(128)

--- public/static-sql/landlord.sql
+++ public/static-sql/landlord.sql
@@ -1,6 +1,6 @@
 CREATE TABLE landlord
 (
-    landlord_id Integer PRIMARY KEY,
+    landlord_id varchar(128) PRIMARY KEY,
     landlord_name varchar(20) NOT NULL,
     email varchar(20) NOT NULL UNIQUE,
     phone_number varchar(12)
