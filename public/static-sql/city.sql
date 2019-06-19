188bd7f66061317572c8bb575db70b2d4a3e408b
Daniil Belikau <daniilbelikau@Daniils-MBP.hitronhub.home>
2019-06-18 23:11:52 -0700

    made the schema consistent with original specification

--- public/static-sql/city.sql
+++ public/static-sql/city.sql
@@ -11,3 +11,8 @@ INSERT INTO city VALUES ("0002", "Richmond", "BC");
 INSERT INTO city VALUES ("0003", "Langley", "BC");
 INSERT INTO city VALUES ("0004", "Kelowna", "BC");
 INSERT INTO city VALUES ("0005", "Surrey", "BC");
+INSERT INTO city VALUES ("0006", "Abbotsford", "BC");
+INSERT INTO city VALUES ("0007", "Whistler", "BC");
+INSERT INTO city VALUES ("0008", "Hope", "BC");
+INSERT INTO city VALUES ("0009", "Ottawa", "ON");
+INSERT INTO city VALUES ("0010", "Toronto", "ON");
--- public/static-sql/province.sql
+++ public/static-sql/province.sql
@@ -4,6 +4,11 @@ province_name VarChar(2) PRIMARY KEY
 
 INSERT INTO province VALUES ("BC");
 INSERT INTO province VALUES ("AB");
-INSERT INTO province VALUES ("YK");
+INSERT INTO province VALUES ("YT");
 INSERT INTO province VALUES ("ON");
 INSERT INTO province VALUES ("QC");
+INSERT INTO province VALUES ("NT");
+INSERT INTO province VALUES ("NU");
+INSERT INTO province VALUES ("MB");
+INSERT INTO province VALUES ("NL");
+INSERT INTO province VALUES ("SK");
